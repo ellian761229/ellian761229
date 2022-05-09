7946c86a2c3f05d54da9ed792102a86465534300
ellian76 <85388799+ellian76@users.noreply.github.com>
2022-05-09 09:40:08 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -127,8 +127,8 @@ class CarInterface(CarInterfaceBase):
       max_lat_accel = 3.2
       ret.lateralTuning.torque.kp = 2.0 / max_lat_accel
       ret.lateralTuning.torque.kf = 1.0 / max_lat_accel
-      ret.lateralTuning.torque.ki = 0.5 / max_lat_accel
-      ret.lateralTuning.torque.friction = 0.1
+      ret.lateralTuning.torque.ki = 0.4 / max_lat_accel
+      ret.lateralTuning.torque.friction = 0.0
 
       ret.lateralTuning.torque.kd = 0.0
       ret.lateralTuning.torque.deadzone = 0.0

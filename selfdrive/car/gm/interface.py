411eeeb292a7e206d9554ff380202434381ac610
ellian76 <85388799+ellian76@users.noreply.github.com>
2022-05-27 08:53:00 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -86,13 +86,13 @@ class CarInterface(CarInterfaceBase):
     tire_stiffness_factor = 0.48
 
     ret.minSteerSpeed = 11 * CV.KPH_TO_MS
-    ret.steerRateCost = 0.35 # def : 2.0
-    ret.steerActuatorDelay = 0.20  # 조향 커브 미리 반응속도
+    ret.steerRateCost = 0.3625 # def : 2.0
+    ret.steerActuatorDelay = 0.18  # 조향 커브 미리 반응속도
 
     ret.minEnableSpeed = -1
     ret.mass = 1625. + STD_CARGO_KG
     ret.wheelbase = 2.60096
-    ret.steerRatio = 17.6  # 조향 힘 반응속도
+    ret.steerRatio = 17.7  # 조향 힘 반응속도
     ret.steerRatioRear = 0.
     ret.centerToFront = ret.wheelbase * 0.49 # wild guess
     ret.disableLateralLiveTuning = True
